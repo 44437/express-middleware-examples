afb68e5ff3d7b8abc7a61821de4bda131258a1fa
44437 <ercantopuz33@gmail.com>
2024-09-14 19:59:26 +0300

    Render some routes case-sensitive

--- router-level.js
+++ router-level.js
@@ -5,6 +5,8 @@ import {productsRouter} from "./routes/products.js";
 const app = express();
 const MAIN_PORT = 8080;
 
+app.set('case sensitive routing', true);
+
 app.listen(MAIN_PORT, () => {
   console.log(`The app is listening on the port ${MAIN_PORT}`);
 });
--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-const router = express.Router()
+const router = express.Router({caseSensitive: true})
 
 router.get("/", (req, res) =>{
   res.send([{name: "product"}]);
